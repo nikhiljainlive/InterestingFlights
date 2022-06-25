b86d51e5e50261d519cf27f2a28d548310501013
nikhiljainlive <nikhiljain5195@gmail.com>
2022-06-25 23:43:19 +0530

    Initialized project

---
+++ app/src/main/java/com/nikhiljain/interesting_flights/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nikhiljain.interesting_flights
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
